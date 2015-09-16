0f1e0ae0dd9d56ae02d3eef4101727c281893982
Travis Montgomery <travisbm@gmail.com>
2015-09-16 17:11:06 -0500

    entered code in seeds.rb to populate table, ran rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Todo.create( {task: "Take out the trash"} )
+Todo.create( {task: "Go grocery shopping"} )
+Todo.create( {task: "Pickup some tartar sauce"} )
+Todo.create( {task: "Pay the phone bill"} )
+Todo.create( {task: "Wash the car"} )
+Todo.create( {task: "Take Tessa dancing"} )
+Todo.create( {task: "Finish Rails homework"} )
+Todo.create( {task: "Practice Ruby"} )
+Todo.create( {task: "Read a book"} )
+Todo.create( {task: "Do the dishes"} )
