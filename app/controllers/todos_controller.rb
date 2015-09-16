c24eb8339cbaca2a2a7a53c4e0f18e9f4f76c91c
Travis Montgomery <travisbm@gmail.com>
2015-09-16 17:30:17 -0500

    defined show method in todos_controller.rb

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,6 +6,12 @@ class TodosController < ApplicationController
   end
 
   def show
+    if Todo.exists?(params[:id])
+      todo = Todo.find(params[:id])
+      render json: todo.to_json, status: 200
+    else
+      render json: { error_msg: 'Record Not Found!', id: params[:id] }.to_json, status: 404
+    end
   end
 
   def create
